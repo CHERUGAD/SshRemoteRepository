fcf2df65bfaca7802c7bf9583e52b27425265997
CHERUGAD <ravikiranpcherugad@gmail.com>
2025-05-04 18:53:01 +0530

    Updated with functions and unnecessary swapping avoided using the boolean values

--- Bubblesort.py
+++ Bubblesort.py
@@ -1,10 +1,17 @@
+def bubbleSort(my_array):
+    n = len(my_array)
+    print(n)
+    for i in range(n-1):
+        swapped = False
+        for j in range(n-i-1):
+            if my_array[j] >= my_array[j+1]:
+                my_array[j] ,my_array[j+1] = my_array[j+1], my_array[j]
+                swapped = True
+        if not swapped :
+            break
+
+
 my_array = [5,6,3,7,2,4]
-n = len(my_array)
-print(n)
-for i in range(n-1):
-    for j in range(n-i-1):
-        if my_array[j] >= my_array[j+1]:
-            my_array[j] ,my_array[j+1] = my_array[j+1], my_array[j]
+bubbleSort(my_array)
 print("Bubble sorted ",my_array)
-    
         
