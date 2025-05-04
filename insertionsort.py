872225c1f964190084fb52b18b4d561b58a83071
CHERUGAD <ravikiranpcherugad@gmail.com>
2025-05-04 20:48:43 +0530

    Merge issue is resolved

--- insertionsort.py
+++ insertionsort.py
@@ -8,22 +8,5 @@ def insertion_sort(arr):
         if min_idx != i:  
             arr[i], arr[min_idx] = arr[min_idx], arr[i]
 arr = [7,3,2,1]
-selection_sort(arr)
-print(arr)
-    for i in range(1, n):
-        key = arr[i]      
-        j = i - 1
-
-        
-        while j >= 0 and arr[j] > key:
-            arr[j + 1] = arr[j]
-            j -= 1
-
-        arr[j + 1] = key    
-
-        # Print the array after each iteration
-        print(f"After inserting {key}, array is: {arr}")
-
-arr = [7, 3, 2, 1]
 insertion_sort(arr)
 print("Final sorted array:", arr)
